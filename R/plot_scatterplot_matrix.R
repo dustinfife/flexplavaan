e60687ba408b18341946f2375d04540d146a6084
dustinfife <fife.dustin@gmail.com>
2021-11-04 13:28:46 -0400

    plot isn't needed until specific functions+

--- R/plot_scatterplot_matrix.R
+++ R/plot_scatterplot_matrix.R
@@ -1,12 +1,12 @@
 #plot_scatter_matrix(fit_bollen$lavaan, subset=1:3)
-#plot_scatter_matrix(fit_twofactor$lavaan, fit_twofactor_2$lavaan, subset=c("x1", "x2", "x3"))
+#plot_scatter_matrix(fit_twofactor$lavaan, fit_twofactor_2$lavaan, subset=c("x1", "x2", "x3"), model_names=c("a", "b"))
 plot_scatter_matrix = function(object1, object2=NULL, subset=NULL, model_names=NULL) {
   
   object1_l = flexplavaan_to_lavaan(object1)
   object2_l = flexplavaan_to_lavaan(object2)
-  
+ 
   # specify subsets
-  d = sort_dataset(object1_l, sort_plots = TRUE, plot="all") %>% 
+  d = sort_dataset(object1_l, sort_plots = TRUE) %>% 
     dplyr::select(dplyr::all_of(get_subset(lavNames(object1_l),
                              subset)))
 
@@ -24,3 +24,10 @@ plot_scatter_matrix = function(object1, object2=NULL, subset=NULL, model_names=N
   return(p + labs(title="Trail/DDP Plots", subtitle="Red=Implied, Blue=Observed"))
   
 }
+
+# p = ggpairs(d, legend=legend,
+#               lower = list(continuous = wrap(viz_diagnostics,fit.lavaan = object_l, fit.lavaan2 = object2_l, alpha = .2, plot="disturbance", label_names=nms, invert.map=TRUE,...)),
+#               diag =  list(continuous = wrap(viz_diagnostics,fit.lavaan = object_l, fit.lavaan2 = object2_l, alpha = .2, plot="histogram",   label_names=nms, ...)),
+#               upper = NULL)    
+# if (is.null(object2_l)) {
+#   p = p + labs(title="DD Plots", subtitle="Red=Implied, Blue=Observed")
