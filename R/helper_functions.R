1589e703a7be4ab6ac97cc26330ca6c577df8061
dustinfife <fife.dustin@gmail.com>
2021-11-04 14:24:21 -0400

    converted scatterplot matrix functions to methods

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,3 +1,13 @@
 get_legend = function(object2) {
   if (is.null(object2)) return(NULL) else return(c(1,2))
 }
+
+set_model_class = function(object1_l, object2_l, names, legend, d, nms, plot) {
+  x = list(object1_l=object1_l, 
+           object2_l=object2_l, 
+           names=names, 
+           legend=legend, 
+           nms=nms,
+           d=d)
+  structure(x, class=plot)
+}
--- R/plot_scatterplot_matrix.R
+++ R/plot_scatterplot_matrix.R
@@ -1,6 +1,7 @@
 #plot_scatter_matrix(fit_bollen$lavaan, subset=1:3)
 #plot_scatter_matrix(fit_twofactor$lavaan, fit_twofactor_2$lavaan, subset=c("x1", "x2", "x3"), model_names=c("a", "b"))
-plot_scatter_matrix = function(object1, object2=NULL, subset=NULL, model_names=NULL) {
+# get rid of model_names argument by making it an attribute??
+plot_scatter_matrix = function(object1, object2=NULL, subset=NULL, model_names=NULL, plot="model", ...) {
   
   object1_l = flexplavaan_to_lavaan(object1)
   object2_l = flexplavaan_to_lavaan(object2)
@@ -16,18 +17,44 @@ plot_scatter_matrix = function(object1, object2=NULL, subset=NULL, model_names=N
   ## get names
   nms = get_and_check_names(model_names, object1_l, object2_l)
   
-  p = ggpairs(d, legend=legend,
+  ## set the class
+  x = set_model_class(object1_l, object2_l, names, legend, d, nms, plot=plot)
+  matrix_plot(x, ...)
+
+}
+
+matrix_plot = function(x, ...) {
+  UseMethod("matrix_plot")
+}
+
+matrix_plot.all = function(x, ...) {
+  p = with(x, ggpairs(d, legend=legend,
               lower = list(continuous = wrap(viz_diagnostics,fit.lavaan = object1_l, fit.lavaan2 = object2_l, alpha = .2, plot="disturbance", label_names=nms, invert.map=TRUE)),
               upper = list(continuous = wrap(viz_diagnostics,fit.lavaan = object1_l, fit.lavaan2 = object2_l, alpha = .2, plot="trace",       label_names=nms)),
               diag  = list(continuous = wrap(viz_diagnostics,fit.lavaan = object1_l, fit.lavaan2 = object2_l, alpha = .2, plot="histogram",   label_names=nms)))
-  if (!is.null(object2_l)) return(p)
+           )
+  if (!is.null(x$object2_l)) return(p)
   return(p + labs(title="Trail/DDP Plots", subtitle="Red=Implied, Blue=Observed"))
-  
 }
 
-# p = ggpairs(d, legend=legend,
-#               lower = list(continuous = wrap(viz_diagnostics,fit.lavaan = object_l, fit.lavaan2 = object2_l, alpha = .2, plot="disturbance", label_names=nms, invert.map=TRUE,...)),
-#               diag =  list(continuous = wrap(viz_diagnostics,fit.lavaan = object_l, fit.lavaan2 = object2_l, alpha = .2, plot="histogram",   label_names=nms, ...)),
-#               upper = NULL)    
-# if (is.null(object2_l)) {
-#   p = p + labs(title="DD Plots", subtitle="Red=Implied, Blue=Observed")
+matrix_plot.trail = function(x, ...) {
+  p = with(x, ggpairs(d, legend=legend,
+              lower = NULL,
+              upper = list(continuous = wrap(viz_diagnostics,fit.lavaan = object1_l, fit.lavaan2 = object2_l, alpha = .2, plot="trace",       label_names=nms)),
+              diag  = list(continuous = wrap(viz_diagnostics,fit.lavaan = object1_l, fit.lavaan2 = object2_l, alpha = .2, plot="histogram",   label_names=nms)))
+  )
+  if (!is.null(x$object2_l)) return(p)
+  return(p + labs(title="Trail/DDP Plots", subtitle="Red=Implied, Blue=Observed"))
+}
+
+matrix_plot.ddp = function(x, ...) {
+
+  p = with(x, ggpairs(d, legend = rev(c(1,2)),
+              lower = list(continuous = wrap(viz_diagnostics,fit.lavaan = object1_l, fit.lavaan2 = object2_l, alpha = .2, plot="disturbance", label_names=nms, invert.map=TRUE)),
+              upper = NULL,
+              diag  = list(continuous = wrap(viz_diagnostics,fit.lavaan = object1_l, fit.lavaan2 = object2_l, alpha = .2, plot="histogram",   label_names=nms)))
+  )
+
+  if (!is.null(x$object2_l)) return(p)
+  return(p + labs(title="Trail/DDP Plots", subtitle="Red=Implied, Blue=Observed"))
+}
