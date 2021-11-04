dee11dc858e8252894da24d9cdc26c812465b2a0
dustinfife <fife.dustin@gmail.com>
2021-11-04 14:30:29 -0400

    simplified set_model_class to fewer arguments for testing

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -2,12 +2,7 @@ get_legend = function(object2) {
   if (is.null(object2)) return(NULL) else return(c(1,2))
 }
 
-set_model_class = function(object1_l, object2_l, names, legend, d, nms, plot) {
-  x = list(object1_l=object1_l, 
-           object2_l=object2_l, 
-           names=names, 
-           legend=legend, 
-           nms=nms,
-           d=d)
+set_model_class = function(..., plot) {
+  x = list(...)
   structure(x, class=plot)
 }
