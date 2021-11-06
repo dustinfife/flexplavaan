b748912c69c120bebee13640bdd30e43f098b9ea
dustinfife <fife.dustin@gmail.com>
2021-11-06 10:46:08 -0400

    passing tests

--- tests/testthat/test-helper_functions.R
+++ tests/testthat/test-helper_functions.R
@@ -57,3 +57,16 @@ test_that("get_subset works", {
 test_that("block_model_residuals works", {
   expect_true(block_model_residuals(flexplavaan_to_lavaan(fit_bollen))[1]==11)
 })
+
+test_that("random_var_name works", {
+  set.seed(23232)
+  expect_true(random_var_name()=="huhpz")
+  expect_true(random_var_name_check(varnames=letters[1:5])=="dlzoi")
+})
+
+test_that("find_latents_for_observed works", {
+  expect_true(find_latents_for_observed(1, flexplavaan_to_lavaan(fit_bollen))=="Eta1")
+  expect_true(length(find_latents_for_observed(3, flexplavaan_to_lavaan(fit_twofactor_2)))==2)
+})
+
+
