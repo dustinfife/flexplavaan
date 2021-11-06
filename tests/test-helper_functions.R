bfb2fd29f3edbd2278ecb5d11282e374162a87dd
dustinfife <fife.dustin@gmail.com>
2021-11-06 09:36:01 -0400

    organizing helper_functions and tests

--- tests/test-helper_functions.R
+++ tests/test-helper_functions.R
@@ -20,3 +20,35 @@ test_that("residual_from_latents works", {
 test_that("flexplavaan_to_lavaan works", {
   expect_true(class(flexplavaan_to_lavaan(fit_bollen))[1], "lavaan")
 })
+
+test_that("check_formula_in_data works", {
+  expect_null( check_formula_in_data(hogwarts_survival, potions~survived + magic_skills))
+  expect_error(check_formula_in_data(hogwarts_survival, potions~survived + magix_skills))  
+})
+
+test_that("get_legend works", {
+  expect_null(get_legend(NULL))
+  expect_equal(get_legend(1), c(1,2))
+})
+
+test_that("get_names works", {
+  expect_true(length(get_names(flexplavaan_to_lavaan(fit_bollen)))==2)
+  expect_true(get_names(flexplavaan_to_lavaan(fit_bollen))[[1]][1] == "y1")
+  expect_true(get_names(flexplavaan_to_lavaan(fit_bollen))[[2]][1] == "Eta1")
+})
+
+test_that("check_models works", {
+  expect_null(check_models(flexplavaan_to_lavaan(force_fit), flexplavaan_to_lavaan(force_cross)))
+  expect_error(check_models(flexplavaan_to_lavaan(fit_twofactor), flexplavaan_to_lavaan(fit_bollen)))
+  expect_null(check_models(fit_twofactor))
+  expect_error(check_models(flexplavaan_to_lavaan(force_fit), flexplavaan_to_lavaan(force_exp)))
+  expect_error(check_models(flexplavaan_to_lavaan(force_exp), flexplavaan_to_lavaan(force_fit)))
+})
+
+test_that("get_subset works", {
+  expect_true(length(get_subset(letters[1:10], NULL))==10)
+  expect_error(get_subset(letters[1:10], 11))
+  expect_true(get_subset(letters[1:10], 3)=="c")
+  expect_error(get_subset(letters[1:10], "q"))
+  expect_true(length(get_subset(letters[1:10], c("a", "b")))==2)
+})
