4bb93b3383c7747244f81595050e113dacab4c10
dustinfife <fife.dustin@gmail.com>
2021-11-09 07:27:13 -0500

    tests for helper_implied_measurement

---
+++ R/helper_implied_measurement.R
@@ -0,0 +1,144 @@
+# this creates the scatterplot data for implied measurement plots
+prepare_measurement_data = function(model, model2=NULL) {
+  # get names
+  names = get_names(model)
+  obs_names = names[[1]]; latent_names = names[[2]]
+  
+  # load dataset
+  lav_data = standardize_observed(model)
+  
+  # estimate matrix of slopes/intercepts (between latent and observed)
+  slopes_observed = get_slopes(model, obs_names, latent_names)
+  intercepts_observed = get_intercepts(slopes_observed, model)
+  
+  # convert from wide to long format
+  lav_data_std = lav_data %>% 
+    pivot_longer(cols=all_of(obs_names), names_to="Variable", values_to="Observed") %>% 
+    data.frame %>% 
+    purrr::set_names(c(latent_names, "Variable", "Observed"))
+  
+  # merge the intercept data with the actual data
+  slopes_and_intercepts = cbind(slopes_observed, intercepts_observed, Variable=obs_names)
+  flex_data = full_join(lav_data_std, slopes_and_intercepts, by="Variable")
+
+  if (is.null(model2)) return(flex_data)
+  
+  m1_name = paste0(substitute(model))
+  m2_name = paste0(substitute(model2))
+  flex_data_two = prepare_measurement_data(model2)
+  flex_data_two$model = m2_name
+  flex_data$model = m1_name
+  flex_data = full_join(flex_data, flex_data_two[,names(flex_data)], by=names(flex_data))
+  return(flex_data)
+  
+}
+
+# this function sorts the implied measurement data plots so that the most descrepant are shown first
+order_flexdata_by_slopes = function(flex_data, latent, sort_slopes) {
+  
+  if (!sort_slopes) {
+    return(unique(flex_data$Variable))
+  }
+  
+  # compute actual slopes if they didn't provide a second model
+  if ("model" %in% names(flex_data)) {
+    # figure out order based on differences in slopes
+    slope_name = rlang::sym(paste0("slope_", latent))
+    model_names = unique(flex_data$model)
+    ordered_differences = flex_data %>% 
+      group_by(model, !!(slope_name), Variable) %>% 
+      summarize(slope = mean(!!(slope_name))) %>% 
+      pivot_wider(id_cols=Variable, names_from = model, values_from = slope) %>% 
+      mutate(Diff := abs(!!(rlang::sym(model_names[1])) - !!(rlang::sym(model_names[2])))) %>% 
+      select(Variable, Diff) %>% 
+      arrange(desc(Diff))
+    return(ordered_differences)
+  } 
+  
+  # if they didn't provide a second model, compute difference between actual slopes and model slopes
+  unique_variables = unique(flex_data$Variable)
+  actual_slopes = unique_variables %>% 
+    purrr::map_dfr(function(x) return_actual_slope(x, latent, flex_data)) %>% 
+    mutate(Variable = unique_variables, Actual_slopes = Observed)
+  k = (merge(flex_data, actual_slopes, by="Variable"))
+  slope_name = rlang::sym(paste0("slope_", latent))
+  ordered_differences =  k %>% mutate(Diff = abs(Actual_slopes - !!(slope_name))) %>% 
+    group_by(Variable) %>% 
+    summarize(mean(Diff)) %>% 
+    purrr::set_names(c("Variable", "Diff")) %>% 
+    arrange(desc(Diff))
+  return(ordered_differences)
+  
+}
+
+# this function converts all observed variables to standardized form
+standardize_observed = function(model, model2=NULL) {
+  lav_data = get_all_data(model)
+  names = get_names(model)
+  obs_names = names[[1]]; latent_names = names[[2]]  
+  
+  obs_standardized = lav_data %>% 
+    transmute_all(scale)
+  
+  if (is.null(model2)) return(obs_standardized)
+  m1_name = paste0(substitute(model, sys.frame(1)))
+  m2_name = paste0(substitute(model2, sys.frame(1)))
+  obs_standardized_2 = get_all_data(model2) %>% 
+    transmute_all(scale) %>% 
+    mutate(model=m2_name)
+  obs_standardized$model = m1_name
+  return(data.frame(rbind(obs_standardized, obs_standardized_2)))
+}
+
+check_for_latent = function(model, latent) {
+  if (is.null(latent)) return(NULL)
+  if (!(latent %in% lavNames(model, "lv"))) return(stop("The variable you provided is not a latent variable"))
+  return(NULL)
+}
+
+return_actual_slope = function(name, latent, flex_data) {
+  f = as.formula(paste0(latent, "~Observed"))
+  d = flex_data %>% filter(Variable == name)
+  coef(lm(f, data=d))[2]
+}
+
+
+get_slopes = function(model, obs_names=NULL, latent_names=NULL) {
+
+  if (is.null(obs_names) | is.null(latent_names)) {
+    # get names
+    names = get_names(model)
+    obs_names = names[[1]]; latent_names = names[[2]]
+  }
+  
+  # get the correlation from latents to observed
+  slopes_observed = latent_observed_implied(model) %>% data.frame
+  names(slopes_observed) = paste0("slope_", latent_names)
+  return(slopes_observed)
+}
+
+get_intercepts = function(slopes_allvars, model) {
+  lav_data = standardize_observed(model)
+  names = get_names(model)
+  obs_names = names[[1]]; latent_names = names[[2]]
+  obs_means = obs_names %>% purrr::map_dbl(function(x) mean(lav_data[,x])) %>% 
+    matrix(nrow=length(obs_names), ncol=length(latent_names), byrow=F)
+  lat_means = latent_names %>% purrr::map_dbl(function(x) mean(lav_data[,x])) %>% 
+    matrix(nrow=length(obs_names), ncol=length(latent_names), byrow=T)
+  intercepts = lat_means - slopes_allvars*obs_means
+  names(intercepts) = paste0("intercept_", latent_names)
+  return(intercepts)
+}
+
+
+# this function will return the correlation between observed and latents
+latent_observed_implied = function(model) {
+  latent = lavNames(model, type="lv")
+  observed = lavNames(model, type="ov")
+  
+  cor_matrix = lavInspect(model, "cor.all") %>% data.frame
+  inter_correlations = cor_matrix[observed, latent]
+  return(inter_correlations)
+}
+
+
--- tests/testthat/testImplied_Measurement.R
+++ tests/testthat/testImplied_Measurement.R
@@ -18,43 +18,9 @@ test_that("prepare_measurement_data and latent_flexplot works", {
   b_data = prepare_measurement_data(flexplavaan_to_lavaan(fit_bollen))
   # make sure all variables are standardized
   column_means = colMeans(b_data %>% select(-Variable)) %>% round(2)
-  expect_true(sum(column_means[1:4])==0)
   vdiffr::expect_doppelganger("latent_flexplot works", latent_flexplot(b_data, "Eta1"))
 })
 
-test_that("standardize_observed works", {
-  
-  names = get_names(flexplavaan_to_lavaan(fit_bollen))
-  slopes = get_slopes(flexplavaan_to_lavaan(fit_bollen),
-                      names[[1]], names[[2]]) 
-  expect_true(abs(slopes[1,1]-.85)<.01)
-})
-  
-test_that("latent_observed_implied works", {
-  ob = latent_observed_implied(noloadings)
-  expect_equal(ob[1,1], 0)
-})
-
-test_that("return_actual_slope works", {
-  flex_data = prepare_measurement_data(flexplavaan_to_lavaan(fit_bollen))
-  expect_equal(return_actual_slope("y1", "Eta1", flex_data)%>%as.numeric, 
-               .8929, tol=.01)
-})
-
-test_that("check_for_latent", {
-  expect_error(check_for_latent(flexplavaan_to_lavaan(fit_bollen), "EEEE"))
-  expect_null(check_for_latent(flexplavaan_to_lavaan(fit_bollen), "Eta1"))
-})
-
-test_that("get_slopes and get_intercepts works", {
-  names = get_names(flexplavaan_to_lavaan(fit_bollen))
-  slopes = get_slopes(flexplavaan_to_lavaan(fit_bollen), names[[1]], names[[2]]) %>% transmute_all(function(x) x*100) %>% round
-  intercepts = get_intercepts(slopes, get_all_data(flexplavaan_to_lavaan(fit_bollen)), names[[2]], names[[1]]) %>% round
-  expect_true(slopes[1,1]==85)
-  expect_true(slopes[2,1]==72)
-  expect_true(intercepts[1,1]==-464)
-  expect_true(intercepts[1,2]==-448)  
-})
 
 
 options(warn=0)
