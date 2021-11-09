9150bffc1bff7092377ac064bd1bf09e8f2d4162
dustinfife <fife.dustin@gmail.com>
2021-11-09 08:16:32 -0500

    implied measurement no longer returns a list of plots

--- tests/testthat/testImplied_Measurement.R
+++ tests/testthat/testImplied_Measurement.R
@@ -5,13 +5,13 @@ set.seed(1212)
 
 
 test_that("implied_measurement works", {
-  vdiffr::expect_doppelganger("implied_measurement plot works", implied_measurement(fit_bollen, latent="Eta1")[[1]])
-  vdiffr::expect_doppelganger("implied_measurement with diff limit works", implied_measurement(fit_bollen, latent="Eta1", limit=3)[[1]])
+  vdiffr::expect_doppelganger("implied_measurement plot works", implied_measurement(fit_bollen, latent="Eta1"))
+  vdiffr::expect_doppelganger("implied_measurement with diff limit works", implied_measurement(fit_bollen, latent="Eta1", limit=3))
   vdiffr::expect_doppelganger("implied_measurement for two models", 
                       implied_measurement(force_fit, 
-                                          force_cross, "Force")[[1]])
+                                          force_cross, "Force"))
   #vdiffr::expect_doppelganger("implied_measurement for two non-nested", 
-  #                    implied_measurement(force_fit, force_exp, "Jedi")[[1]])
+  #                    implied_measurement(force_fit, force_exp, "Jedi"))
 })
 
 test_that("prepare_measurement_data and latent_flexplot works", {
