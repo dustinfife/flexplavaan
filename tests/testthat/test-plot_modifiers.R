53147d01806cb60a13c4c445f4a9ef279bbc351e
dustinfife <fife.dustin@gmail.com>
2021-11-09 08:17:03 -0500

    added tests for modify_latent and modify_smooth

--- tests/testthat/test-plot_modifiers.R
+++ tests/testthat/test-plot_modifiers.R
@@ -25,3 +25,14 @@ test_that("modify_model_names works", {
   vdiffr::expect_doppelganger("modify_model_names two plots", plot_scatter_matrix(fit1, fit2, subset=1:3, plot="all") %>% modify_model_names(c("a", "b")))
   expect_error(modify_model_names(1, "a"))
 })
+
+p = implied_measurement(small, small_mis)
+test_that("modify_latent works", {
+  vdiffr::expect_doppelganger("modify_latent for implied measurement", p %>% modify_latent("f2"))
+})
+
+test_that("modify_smooth works", {
+  vdiffr::expect_doppelganger("modify_smooth for implied measurement", p %>% modify_smooth(method="quadratic"))
+  visualize(small, plot="trail", subset=1:3) %>% modify_smooth(method="lm")
+                              
+})
