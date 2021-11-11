4ebfd1bb0f0f297d74e231988f6a0506163327da
dustinfife <fife.dustin@gmail.com>
2021-11-11 07:48:11 -0500

    changed name of tests for plot_latent

--- tests/testthat/test-plot_latent.R
+++ tests/testthat/test-plot_latent.R
@@ -1,10 +1,10 @@
-context("testplot_latent plots")
+context("plot_latent")
 set.seed(2323)
 options(warn=-1)
 require(lavaan)
 
 test_that("latent_plot works", {
-  vdiffr::expect_doppelganger("simple latent_plot",latent_plot(fit_bollen))
+  vdiffr::expect_doppelganger("simple latent_plot",latent_plot(small, small_mis))
 })  
 
 test_that("return_alpha works", {
