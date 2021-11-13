abc8761b3fd7efad56f612a3147d813e8dae673b
dustinfife <fife.dustin@gmail.com>
2021-11-13 07:09:25 -0500

    finished residual_plots unit tests

--- tests/testthat/test-plot_residuals.R
+++ tests/testthat/test-plot_residuals.R
@@ -3,6 +3,6 @@ context("plot_residuals works")
 set.seed(2323)
 
 test_that("residual_plots works", {
-  residual_plots(small)
-  residual_plots(small, small_mis)
+  vdiffr::expect_doppelganger("residual_plot", residual_plots(small))
+  vdiffr::expect_doppelganger("residual_plot two models", residual_plots(small, small_mis))
 })
